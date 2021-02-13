ea97eef60d31c19be853369a21566cd04b2be980
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-02-13 22:21:40 +0400

    Fix AirlineDto if empty input object

--- src/js/Dto/AirlineDto.js
+++ src/js/Dto/AirlineDto.js
@@ -1,11 +1,15 @@
 export default class AirlineDto {
     constructor(obj) {
-        this.code = obj.code;
+        this.code = obj?.code || undefined;
         this.name = obj?.name || obj?.name_translations?.en || undefined;
         this.nameEn = obj?.name_translations?.en || obj?.nameEn || undefined;
     }
 
     getLogoUrl(width = 100, height = 100) {
+        if (undefined === this.code) {
+            return "";
+        }
+
         return `http://pics.avs.io/${width}/${height}/${this.code}.png`;
     }
 }
