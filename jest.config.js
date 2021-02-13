71e9f75dcad676a9042961339f091c038af3384b
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-02-13 22:15:59 +0400

    Jest config.

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    clearMocks: true,
+    collectCoverageFrom: ["src/**/*.js"],
+    coverageDirectory: "coverage",
+    moduleFileExtensions: ["js"],
+    testMatch: ["**/__test__/**/*.js?(x)", "**/?(.*)+(spec|test).js?(x)"],
+    transformIgnorePatterns: ["<rootDir>/node_modules/"],
+    transform: {
+        ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
+        "^.+\\.(js|jsx)?$": "babel-jest"
+    },
+    verbose: true,
+};
