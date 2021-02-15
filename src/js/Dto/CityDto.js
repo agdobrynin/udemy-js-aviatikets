b1bee90c132057931e039ec5ea910c70a0dbc2b5
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-02-15 16:05:35 +0400

    Fix test for AirlineDto with mocks data

--- src/js/Dto/CityDto.js
+++ src/js/Dto/CityDto.js
@@ -1,6 +1,6 @@
 export default class CityDto {
     constructor(city, countryName = "") {
-        this.code = city.code;
+        this.code = city.code || undefined;
         this.countryCode = city?.country_code || city?.countryCode || undefined;
         this.name = city?.name || city?.name_translations?.en || city.code;
         this.countryName = countryName || city?.countryName || undefined;
--- src/js/Dto/__test__/AirlineDto.spec.js
+++ src/js/Dto/__test__/AirlineDto.spec.js
@@ -1,48 +1,51 @@
 import AirlineDto from "@/Dto/AirlineDto";
-
-const obj = {
-    code: "MSK",
-    name: "Москва",
-    nameEn: "Moscow",
-};
-
-const objNoRus = {
-    name_translations: {
-        en: "Moskwa",
-    }
-}
+import airlines from "@/__test__/moks/airlines.json";
 
 describe("Test Airline Dto class", () => {
-    it("check russian name", ()=> {
-        expect((new AirlineDto(obj)).name).toBe(obj.name);
-    });
-
-    it("check english name", ()=> {
-        expect((new AirlineDto(obj)).nameEn).toBe(obj.nameEn);
-    });
 
     it("check no russian name", ()=> {
-        expect((new AirlineDto(objNoRus)).name).toBe(objNoRus.name_translations.en);
+        const obj = airlines[0];
+        const dto = new AirlineDto(obj);
+
+        expect(dto).toBeInstanceOf(AirlineDto);
+        expect(dto.name).toBe(obj.name_translations.en);
+        expect(dto.code).toBe(obj.code);
+    });
+
+    it("check russian name", ()=> {
+        const obj = airlines[1];
+        const dto = new AirlineDto(obj);
+
+        expect(dto).toBeInstanceOf(AirlineDto);
+        expect(dto.name).toBe(obj.name);
+        expect(dto.code).toBe(obj.code);
     });
 
     it("check empty input", ()=> {
         const dto = new AirlineDto({});
+
+        expect(dto).toBeInstanceOf(AirlineDto);
         expect(dto.code).toBe(undefined);
         expect(dto.name).toBe(undefined);
     });
 
     it("logo airline default", ()=> {
+        const obj = airlines[0];
         const dto = new AirlineDto(obj);
+
         expect(dto.getLogoUrl()).toBe(`http://pics.avs.io/100/100/${obj.code}.png`);
     });
 
     it("logo airline with 150 pix , height 200 pix", ()=> {
+        const obj = airlines[1];
         const dto = new AirlineDto(obj);
+
         expect(dto.getLogoUrl(150, 200)).toBe(`http://pics.avs.io/150/200/${obj.code}.png`);
     });
 
     it("logo airline on empty object", ()=> {
         const dto = new AirlineDto({});
+
         expect(dto.getLogoUrl()).toBe("");
     });
 });
