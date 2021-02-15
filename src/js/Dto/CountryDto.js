e3ce3c3803aa9f20454520165beec5efd7f1f98c
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-02-15 17:05:22 +0400

    Add new property for CountryDto.nameEn - english name

--- src/js/Dto/CountryDto.js
+++ src/js/Dto/CountryDto.js
@@ -1,6 +1,7 @@
 export default class CountryDto {
     constructor(obj) {
-        this.code = obj.code;
-        this.name = obj?.name || obj?.name_translations?.en || obj.code;
+        this.code = obj.code || undefined;
+        this.name = obj?.name || obj?.name_translations?.en || obj.code || undefined;
+        this.nameEn = obj?.name_translations?.en || obj.code;
     }
 }
